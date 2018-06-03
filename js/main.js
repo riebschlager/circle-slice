7e335ca133b0ab080a57efc396f7443e60ac6cae
Chris Riebschlager <criebschlager@dimin.com>
2018-06-02 21:47:06 -0500

    add fitting and drag drop

--- js/main.js
+++ js/main.js
@@ -15,14 +15,29 @@ gui.add(slice, 'rotate', 1, 50);
 img.src = './img/beach-calm-clouds-378271.jpg';
 img.addEventListener('load', slice.init);
 
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
 function Slice() {
   this.steps = 10;
   this.rotate = 10;
 
   const draw = () => {
+    let area = { x: 0, y: 0, width: canvas.width, height: canvas.height };
+    let rect = { x: 0, y: 0, width: img.width, height: img.height };
+    let transform = fit(rect, area, { cover: true });
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'rgba(255,255,255,0.7)';
+    ctx.drawImage(
+      img,
+      transform.x,
+      transform.y,
+      transform.width,
+      transform.height
+    );
+    ctx.fillStyle = 'rgba(255,255,255,0.25)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     let sizeStep = canvas.height / 2 / this.steps;
     for (let i = 0; i < this.steps; i++) {
@@ -38,13 +53,13 @@ function Slice() {
       );
       ctx.clip();
       ctx.translate(canvas.width / 2, canvas.height / 2);
-      ctx.rotate(i * this.rotate * Math.PI / 180);
+      ctx.rotate((i + 1) * this.rotate * Math.PI / 180);
       ctx.drawImage(
         img,
-        -canvas.width / 2,
-        -canvas.height / 2,
-        canvas.width,
-        canvas.height
+        -canvas.width / 2 + transform.x,
+        -canvas.height / 2 + transform.y,
+        transform.width,
+        transform.height
       );
       ctx.restore();
     }
@@ -54,4 +69,35 @@ function Slice() {
   this.init = () => {
     draw();
   };
+}
+
+function dropHandler(ev) {
+  ev.preventDefault();
+
+  if (ev.dataTransfer.items) {
+    for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+      if (ev.dataTransfer.items[i].kind === 'file') {
+        let file = ev.dataTransfer.items[i].getAsFile();
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = function() {
+          img.src = reader.result;
+        };
+      }
+    }
+  }
+
+  removeDragData(ev);
+}
+
+function dragOverHandler(ev) {
+  ev.preventDefault();
+}
+
+function removeDragData(ev) {
+  if (ev.dataTransfer.items) {
+    ev.dataTransfer.items.clear();
+  } else {
+    ev.dataTransfer.clearData();
+  }
 }
